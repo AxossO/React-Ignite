cfcf6709fe30a33b87f93d46a75dc863906d3bb2
AxossO <ahmadsayed142d@gmail.com>
2023-09-02 00:30:05 +0300

    Uploading

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/my-app">
+      <BrowserRouter basename="/React-Ignite">
         <App />
       </BrowserRouter>
     </Provider>
